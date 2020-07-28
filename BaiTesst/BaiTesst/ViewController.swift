207b5bc2673368ddbe75f13ecefc977ab5ca21a9
0966474398 <59565274+0966474398@users.noreply.github.com>
2020-07-28 19:44:14 +0700

    Baitest

---
+++ BaiTesst/BaiTesst/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BaiTesst
+//
+//  Created by Nguyen Quoc Dich on 7/27/20.
+//  Copyright © 2020 Nguyen.Quoc.Dich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Button1.layer.cornerRadius = 20
+        //Button2.layer.cornerRadius = 20
+        Button3.layer.cornerRadius = 20
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
